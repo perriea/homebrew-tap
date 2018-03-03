1a4439c8e2a1ad475741417cef0e2e4c6b1310b1
goreleaserbot <goreleaser@carlosbecker.com>
2018-03-03 16:13:50 +0100

    tfversion version 0.1.4

--- Formula/tfversion.rb
+++ Formula/tfversion.rb
@@ -1,15 +1,15 @@
 class Tfversion < Formula
-    desc "Switcher Terraform as fast and easily as possible"
-    homepage "https://aurelienperrier.com/tfversion"
-    url "https://github.com/perriea/tfversion/releases/download/0.1.3/tfversion_darwin_amd64.zip"
-    version "0.1.3"
-    sha256 "96f1fc39011699b4291d230a332a5a0242757d615a28307323272525d9c3a3c6"
-  
-    def install
-      bin.install "tfversion"
-    end
-  
-    test do
-      system "#{bin}/tfversion version"
-    end
+  desc "Install Terraform binaries as fast and easily as possible"
+  homepage "https://aurelienperrier.com/tfversion"
+  url "https://github.com/perriea/tfversion/releases/download/0.1.4/tfversion_Darwin_x86_64.tar.gz"
+  version "0.1.4"
+  sha256 "490bf9424ee3d0f44cee01caba2d2edc1f7fb3a6d6e2f0d49215585b7cd6dda8"
+
+  def install
+    bin.install "tfversion"
+  end
+
+  test do
+    system "#{bin}/goreleaser -v"
+  end
 end
