5f32015e4a3d3fa2a8ef20198e803ae3e8953e15
goreleaserbot <goreleaser@carlosbecker.com>
2018-03-03 16:24:25 +0100

    tfversion version 0.1.4

--- Formula/tfversion.rb
+++ Formula/tfversion.rb
@@ -3,13 +3,13 @@ class Tfversion < Formula
   homepage "https://aurelienperrier.com/tfversion"
   url "https://github.com/perriea/tfversion/releases/download/0.1.4/tfversion_Darwin_x86_64.tar.gz"
   version "0.1.4"
-  sha256 "490bf9424ee3d0f44cee01caba2d2edc1f7fb3a6d6e2f0d49215585b7cd6dda8"
+  sha256 "8bc650459b2b23870a859a0666bc251b1c8ac0362f88f08d7065e48239211ac5"
 
   def install
     bin.install "tfversion"
   end
 
   test do
-    system "#{bin}/goreleaser -v"
+    system "#{bin}/tfversion version"
   end
 end
