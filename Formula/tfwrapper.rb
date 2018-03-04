f5e360328344c5bfa52d2276708cb15e729c0c4c
goreleaserbot <goreleaser@carlosbecker.com>
2018-03-04 21:58:38 +0100

    tfwrapper version 0.2.0

--- Formula/tfwrapper.rb
+++ Formula/tfwrapper.rb
@@ -1,9 +1,9 @@
 class Tfwrapper < Formula
   desc "Apply best practice of Terraform as fast and easily as possible"
   homepage "https://aurelienperrier.com/tfwrapper"
-  url "https://github.com/perriea/tfwrapper/releases/download/0.1.0/tfwrapper_Darwin_x86_64.tar.gz"
-  version "0.1.0"
-  sha256 "9abe7bd25e25e4ee956c9ff87620283e1d26cd6ddcdb91d74d73394734a31801"
+  url "https://github.com/perriea/tfwrapper/releases/download/0.2.0/tfwrapper_Darwin_x86_64.tar.gz"
+  version "0.2.0"
+  sha256 "9aa48e53924f001783f1ffbe28ae86effe7cc876ece23889fbff0541610b8d77"
 
   def install
     bin.install "tfwrapper"
