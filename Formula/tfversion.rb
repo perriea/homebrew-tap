7b2980f5c7368e400d106892acf05cd754942930
goreleaserbot <goreleaser@carlosbecker.com>
2018-04-13 10:43:38 +0200

    Brew formula update for tfversion version 0.1.5

--- Formula/tfversion.rb
+++ Formula/tfversion.rb
@@ -1,9 +1,9 @@
 class Tfversion < Formula
   desc "Install Terraform binaries as fast and easily as possible"
   homepage "https://aurelienperrier.com/tfversion"
-  url "https://github.com/perriea/tfversion/releases/download/0.1.4/tfversion_Darwin_x86_64.tar.gz"
-  version "0.1.4"
-  sha256 "7ea496345876aca72a9293108121c4a420b86956ab186c0d433b6b35f7a01131"
+  url "https://github.com/perriea/tfversion/releases/download/0.1.5/tfversion_Darwin_x86_64.tar.gz"
+  version "0.1.5"
+  sha256 "3b8667ab6d6936484f9da0220a24a9d1570d202d194ba690f4018b3b5e77e83f"
 
   def install
     bin.install "tfversion"
